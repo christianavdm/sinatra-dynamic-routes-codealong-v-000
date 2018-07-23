0a8840b317749f31a377c1eb7454b262bf811e4c
Christiana Vandermale <christianavdm@gmail.com>
2018-07-23 23:29:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,4 +20,10 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
+  get "/multiply/:number1/:number2" do
+    @number1 = params[:number1]
+    @number2 = params[:number2]
+    "Hello #{@user_name}!"
+  end
+
 end
